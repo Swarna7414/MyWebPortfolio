4f7e148a18c0405c1559f8a695691cf4fcf83837
Swarna7414 <swarnasaisankar044@gmail.com>
2025-07-04 20:51:21 -0500

    reveting back the fix

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -46,7 +46,7 @@ const Home: React.FC = () => {
             <h1 className="block lg:hidden text-transparent">{""}This is Debash</h1>
             <h1 className="hidden xl:block text-4xl font-semibold mb-2 group">This is {" "}<span className="group-hover:text-blue-500 text-5xl cursor-pointer transition-all duration-300 font-bold relative">Debesh Jha <span className="hidden lg:inline-block lg:group-hover:inline-block ml-2 animate-pulse group-hover:animate-bounce">🤖</span></span></h1>
             <div className="flex flex-col self-center sm:px-3">
-              <h1 className="lg:text-justify sm:text-left text-xl font-medium w-full max-w-4xl xl:mt-12">
+              <h1 className="text-justify text-xl font-medium w-full max-w-4xl xl:mt-12">
                 I'm Visiting Assistant Professor in the Department of Computer Science at the University of South Dakota. My research focuses on AI-driven tools for medical imaging and diagnostics. 
                 I’ve developed models like ColonSegNet and contributed to datasets such as Kvasir-SEG, HyperKvasir, and PolypGen. Recognized among the world’s top 2% scientists by Stanford and Elsevier, I aim to build reliable systems 
                 that support radiologists in diagnosing faster and more accurately. My work spans gastrointestinal imaging, tumor analysis, and predictive modeling for radiation therapy, as well as vision-language models, anomaly detection, 
