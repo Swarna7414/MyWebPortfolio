17c3cb0d3cb1f5436ddd7ed201a21cce57140a74
Swarna7414 <swarnasaisankar044@gmail.com>
2025-07-06 23:59:09 -0500

    Deploying Webpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/PortFolio/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-
-  base:"/DebeshJha-s-Portfolio/"
-
-})
+});
