7fbdf342e98e0fa0cfaa15b17992c6b3c7a1b1fd
Swarna7414 <swarnasaisankar044@gmail.com>
2025-07-08 01:33:36 -0500

    fixed education page

--- src/Pages/Professional.tsx
+++ src/Pages/Professional.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "./ProfessionalScreen";
 import AndN from "./Professional/AandN";
 import AcademicCarrier from "./Professional/AcademicCarrier";
 import Professional from "./Professional/Professioalcarrier";
+import SmallAcademicCarrier from "./Professional/SmallAcademicCarrier";
 
 const ProfessionalCareer: React.FC = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkScreen = () => {
+      setIsLargeScreen(window.innerWidth >= 1024); // Tailwind's `lg` breakpoint = 1024px
+    };
+
+    checkScreen(); // Initial check
+    window.addEventListener("resize", checkScreen); // Listen on resize
+
+    return () => window.removeEventListener("resize", checkScreen);
+  }, []);
+
   return (
     <Carousel>
       <AndN />
-      <AcademicCarrier />
+      {isLargeScreen ? <AcademicCarrier /> : <SmallAcademicCarrier />}
       <Professional />
     </Carousel>
   );
